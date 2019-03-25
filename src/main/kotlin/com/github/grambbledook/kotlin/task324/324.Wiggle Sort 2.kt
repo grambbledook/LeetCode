9faf1151aa0bec837bd2023bcf4d0fef6c7af35c
denis <vodopyanovd@gmail.com>
2019-03-26 01:32:43 +0300

    More on wiggle sort

--- src/main/kotlin/com/github/grambbledook/kotlin/task324/324.Wiggle Sort 2.kt
+++ src/main/kotlin/com/github/grambbledook/kotlin/task324/324.Wiggle Sort 2.kt
@@ -49,18 +49,14 @@ class Solution {
             array[l] = nums[median - i - 1]
             l += 2
         }
-        for (i in 0 until median - 1) {
-            array[r] = nums[nums.size - median + 1 + i]
+        for (i in 0 until median) {
+            array[r] = nums[nums.size - median + i]
             r += 2
         }
         return array
     }
 
 }
-// L L L M M H H H
-// 4 5 5 5 5 6 6 6
-// M H L H L H L M
-// 5 6 5 6 5 6 4 5
 
 fun main(args: Array<String>) {
     exec(listOf(1, 5, 1, 1, 6, 4))
@@ -69,6 +65,7 @@ fun main(args: Array<String>) {
     exec(listOf(1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2))
     exec(listOf(4, 5, 5, 6))
     exec(listOf(4, 5, 5, 5, 5, 6, 6, 6))
+    exec(listOf(5, 3, 1, 2, 6, 7, 8, 5, 5))
 }
 
 private fun exec(nums: List<Int>) {
