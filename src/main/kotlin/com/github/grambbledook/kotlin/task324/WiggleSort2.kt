76e23d1f77a71807092aae0728e4a777d01c46f1
denis <vodopyanovd@gmail.com>
2019-03-28 14:45:21 +0300

    generalizing wiggle sort

--- src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
+++ src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
@@ -1,7 +1,5 @@
 package com.github.grambbledook.kotlin.task324
 
-import com.github.grambbledook.kotlin.common.QuickselectSedgwick
-
 
 class Solution {
     fun wiggleSort(nums: IntArray) {
@@ -11,13 +9,13 @@ class Solution {
     }
 
     private fun wiggle(nums: IntArray) {
-        val array = if (nums.size % 2 == 0) wiggleEven(nums, nums.size / 2 - 1)
-        else wiggleOdd(nums, nums.size / 2)
+        val array = if (nums.size % 2 == 0) wiggleEven(nums, nums.size / 2, 2)
+        else wiggleOdd(nums, nums.size / 2, 1)
 
         array.forEachIndexed { idx, v -> nums[idx] = v }
     }
 
-    private fun wiggleOdd(nums: IntArray, median: Int): IntArray {
+    private fun wiggleOdd(nums: IntArray, median: Int, offset: Int): IntArray {
         val array = IntArray(nums.size)
 
         var l = 1
@@ -29,28 +27,28 @@ class Solution {
             array[l] = nums[i]
             l += 2
         }
-        for (i in 1 until median + 1) {
-            array[r] = nums[median + i]
+        for (i in 0 until median) {
+            array[r] = nums[median + i + offset]
             r += 2
         }
         return array
     }
 
-    private fun wiggleEven(nums: IntArray, median: Int): IntArray {
+    private fun wiggleEven(nums: IntArray, median: Int, offset: Int): IntArray {
         val array = IntArray(nums.size)
 
         var l = 1
         var r = 2
 
-        array[0] = nums[nums.size / 2]
-        array[nums.size - 1] = nums[nums.size / 2 - 1]
+        array[0] = nums[median]
+        array[nums.size - 1] = nums[median - 1]
 
         for (i in 0 until median) {
             array[l] = nums[i]
             l += 2
         }
-        for (i in 2 until median + 1) {
-            array[r] = nums[median + i]
+        for (i in 0 until median) {
+            array[r % nums.size] = nums[median + i]
             r += 2
         }
         return array
