cc04ccd49c3649c25af3da8002c6e20ef5890ac0
denis <vodopyanovd@gmail.com>
2019-03-28 23:10:07 +0300

    generalizing wiggle sort

--- src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
+++ src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
@@ -9,47 +9,26 @@ object Solution {
     }
 
     private fun wiggle(nums: IntArray): IntArray {
-        val array = if (nums.size % 2 == 0) wiggleEven(nums, nums.size / 2 - 1)
-        else wiggleOdd(nums, nums.size / 2)
+        val array = if (nums.size % 2 == 0) wiggle(nums, nums.size / 2 - 1)
+        else wiggle(nums, nums.size / 2 - 1)
 
         array.forEachIndexed { idx, v -> nums[idx] = v }
         return nums
     }
 
-    private fun wiggleOdd(nums: IntArray, median: Int): IntArray {
+    private fun wiggle(nums: IntArray, median: Int): IntArray {
         val array = IntArray(nums.size)
 
         var l = 1
-        var r = 2
+        var r = 0
 
-        array[0] = nums[median]
-
-        for (i in 0 until median) {
+        for (i in 0 until median + 1) {
             array[l] = nums[i]
             l += 2
         }
-        for (i in 1 until median + 1) {
-            array[r] = nums[median + i]
-            r += 2
-        }
-        return array
-    }
-
-    private fun wiggleEven(nums: IntArray, median: Int): IntArray {
-        val array = IntArray(nums.size)
-
-        var l = 1
-        var r = 2
-
-        array[0] = nums[nums.size / 2]
-        array[nums.size - 1] = nums[nums.size / 2 - 1]
-
-        for (i in 0 until median) {
-            array[l] = nums[i]
-            l += 2
-        }
-        for (i in 0 until median) {
-            array[r] = nums[nums.size - median + i]
+        for (i in 1 until median + 2 + nums.size % 2) {
+            val idx = median + i
+            array[r] = nums[idx]
             r += 2
         }
         return array
