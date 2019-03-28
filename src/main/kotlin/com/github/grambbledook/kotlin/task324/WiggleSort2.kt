b60187e12a4822ac96e030b30d3374ece2016a17
denis <vodopyanovd@gmail.com>
2019-03-28 15:35:01 +0300

    this is ok

--- src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
+++ src/main/kotlin/com/github/grambbledook/kotlin/task324/WiggleSort2.kt
@@ -1,21 +1,22 @@
 package com.github.grambbledook.kotlin.task324
 
-
-class Solution {
-    fun wiggleSort(nums: IntArray) {
+object Solution {
+    fun wiggleSort(nums: IntArray): IntArray {
         nums.sortDescending()
-//        QuickselectSedgwick.select(nums, nums.size / 2 )
+//        Quickselect.sort(nums, 0, nums.size -1, nums.size / 2 + 1)
+//        QuickselectSedgwick.select(nums, nums.size / 2 + 1)
         return wiggle(nums)
     }
 
-    private fun wiggle(nums: IntArray) {
-        val array = if (nums.size % 2 == 0) wiggleEven(nums, nums.size / 2, 2)
-        else wiggleOdd(nums, nums.size / 2, 1)
+    private fun wiggle(nums: IntArray): IntArray {
+        val array = if (nums.size % 2 == 0) wiggleEven(nums, nums.size / 2 - 1)
+        else wiggleOdd(nums, nums.size / 2)
 
         array.forEachIndexed { idx, v -> nums[idx] = v }
+        return nums
     }
 
-    private fun wiggleOdd(nums: IntArray, median: Int, offset: Int): IntArray {
+    private fun wiggleOdd(nums: IntArray, median: Int): IntArray {
         val array = IntArray(nums.size)
 
         var l = 1
@@ -27,28 +28,28 @@ class Solution {
             array[l] = nums[i]
             l += 2
         }
-        for (i in 0 until median) {
-            array[r] = nums[median + i + offset]
+        for (i in 1 until median + 1) {
+            array[r] = nums[median + i]
             r += 2
         }
         return array
     }
 
-    private fun wiggleEven(nums: IntArray, median: Int, offset: Int): IntArray {
+    private fun wiggleEven(nums: IntArray, median: Int): IntArray {
         val array = IntArray(nums.size)
 
         var l = 1
         var r = 2
 
-        array[0] = nums[median]
-        array[nums.size - 1] = nums[median - 1]
+        array[0] = nums[nums.size / 2]
+        array[nums.size - 1] = nums[nums.size / 2 - 1]
 
         for (i in 0 until median) {
             array[l] = nums[i]
             l += 2
         }
         for (i in 0 until median) {
-            array[r % nums.size] = nums[median + i]
+            array[r] = nums[nums.size - median + i]
             r += 2
         }
         return array
--- src/test/kotlin/com/github/grambbledook/kotlin/task324/WiggleSortTest.kt
+++ src/test/kotlin/com/github/grambbledook/kotlin/task324/WiggleSortTest.kt
@@ -5,97 +5,110 @@ import org.junit.jupiter.api.Test
 
 
 class WiggleSortTest {
-    val test = Solution()
-
+    val test = Solution
 
     @Test
     fun `test case 1`() {
-        intArrayOf(1, 5, 1, 1, 6, 4).let {
-            test.wiggleSort(it)
+        listOf(1, 5, 1, 1, 6, 4).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 2`() {
-        intArrayOf(1, 3, 2, 2, 3, 1).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(1, 3, 2, 2, 3, 1).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 3`() {
-        intArrayOf(1, 1, 2, 1, 2, 2, 1).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(1, 1, 2, 1, 2, 2, 1).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 4`() {
-        intArrayOf(1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(1, 3, 2, 2, 2, 1, 1, 3, 1, 1, 2).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 5`() {
-        intArrayOf(4, 5, 5, 6).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(4, 5, 5, 6).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 6`() {
-        intArrayOf(4, 5, 5, 5, 5, 6, 6, 6).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(4, 5, 5, 5, 5, 6, 6, 6).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 7`() {
-        intArrayOf(5, 3, 1, 2, 6, 7, 8, 5, 5).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(5, 3, 1, 2, 6, 7, 8, 5, 5).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 8`() {
-        intArrayOf(1, 1, 2, 2, 2, 1).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(1, 1, 2, 2, 2, 1).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 9`() {
-        intArrayOf(2, 3, 3, 2, 2, 2, 1, 1).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(2, 3, 3, 2, 2, 2, 1, 1).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
     @Test
     fun `test case 10`() {
-        intArrayOf(3, 3, 3, 2, 2, 2, 3, 2, 1, 1, 2, 1, 2, 3, 3, 3, 1, 2).let {
-            test.wiggleSort(it)
-            assertWiggled(it)
+        listOf(3, 3, 3, 2, 2, 2, 3, 2, 1, 1, 2, 1, 2, 3, 3, 3, 1, 2).let { orig ->
+            test.wiggleSort(orig.toIntArray()).also {
+                assertWiggled(it, orig)
+            }
         }
     }
 
-    fun assertWiggled(nums: IntArray) {
+    fun assertWiggled(nums: IntArray, orig: List<Int>) {
         var last = nums[0]
-
+        val delta = orig.toMutableList()
         for (i in 1 until nums.size) {
             val invariant = if (i % 2 == 1) last < nums[i] else last > nums[i]
+            delta.remove(last)
             last = nums[i]
 
             Assertions.assertTrue(invariant, "${nums.toList()}. Array size: ${nums.size}")
         }
+        delta.remove(nums.last())
+        Assertions.assertTrue(delta.isEmpty(), "original $orig, wiggled ${nums.toList()} remaining elements $delta")
 
     }
 
