c5f7b2dd234a56ac6a8b43d344fbf9eff2c51c6a
denis <vodopyanovd@gmail.com>
2019-08-11 23:38:56 +0300

    344. Reverse string

---
+++ src/main/java/com/github/grambbledook/java/task344/Solution.java
@@ -0,0 +1,16 @@
+package com.github.grambbledook.java.task344;
+
+public class Solution {
+
+    public void reverseString(char[] s) {
+
+        for (int i = 0; i < s.length / 2; i++) {
+            char c = s[i];
+
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = c;
+        }
+
+    }
+
+}
