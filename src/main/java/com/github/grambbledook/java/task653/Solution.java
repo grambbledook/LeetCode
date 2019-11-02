ee843a3b20b978036e65cf8a96c9c751414da88f
denis <vodopyanovd@gmail.com>
2019-11-02 22:05:52 +0300

    653. Two Sum IV - Input is a BST

---
+++ src/main/java/com/github/grambbledook/java/task653/Solution.java
@@ -0,0 +1,22 @@
+package com.github.grambbledook.java.task653;
+
+import com.github.grambbledook.java.common.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    public boolean findTarget(TreeNode root, int target) {
+        Set<Integer> state = new HashSet<>();
+        return go(root, target, state);
+    }
+
+    private boolean go(TreeNode root, int target, Set<Integer> state) {
+        if (root == null) return false;
+        if (state.contains(root.val)) return true;
+
+        state.add(target - root.val);
+        return go(root.left, target, state) || go(root.right, target, state);
+    }
+}
